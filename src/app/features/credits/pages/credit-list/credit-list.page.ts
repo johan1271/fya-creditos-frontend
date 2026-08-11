719d00e07cb8e890b208e715fb74c3a9c11f17a1
johansrb <johan1271@hotmail.com>
2026-08-11 10:07:44 -0500

    Add credit search page with pagination

---
+++ src/app/features/credits/pages/credit-list/credit-list.page.ts
@@ -0,0 +1,59 @@
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { chevronBackOutline, chevronForwardOutline } from 'ionicons/icons';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonSearchbar,
+  IonSkeletonText,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+
+import { CreditsStore } from '../../services/credits.store';
+
+@Component({
+  selector: 'app-credit-list',
+  templateUrl: 'credit-list.page.html',
+  styleUrls: ['credit-list.page.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonSearchbar,
+    IonCard,
+    IonCardContent,
+    IonSkeletonText,
+    IonButton,
+    IonIcon,
+    CurrencyPipe,
+    DatePipe,
+  ],
+})
+export class CreditListPage implements OnInit {
+  protected readonly store = inject(CreditsStore);
+  protected readonly chevronBackOutline = chevronBackOutline;
+  protected readonly chevronForwardOutline = chevronForwardOutline;
+  protected readonly skeletonRows = Array.from({ length: 4 });
+
+  ngOnInit(): void {
+    this.store.search('');
+  }
+
+  onSearchChange(event: CustomEvent<{ value?: string | null }>): void {
+    this.store.search(event.detail.value ?? '');
+  }
+
+  previousPage(): void {
+    this.store.search(this.store.searchTerm(), this.store.page() - 1);
+  }
+
+  nextPage(): void {
+    this.store.search(this.store.searchTerm(), this.store.page() + 1);
+  }
+}
